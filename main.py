18a0ad88d516602b425e32e45f69eee651c445f0
luigiferrarasinno <rm98047@gmail.com.br>
2023-11-17 17:12:32 -0300

    finalizado

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ medicos={'larissa': '57742',
          'fernando': "98047",
          'a':'a'}
 
-
-
 def adicionar_tarefa():
    
     usuario=input("digite o login:")
@@ -112,9 +110,36 @@ def mostrar_atividades_paciente():
     else:
         print("Paciente não encontrado")
 
+def criar_medico():
+     usuario=input("digite o login:")
+     senha=input("digite a sua senha:")
+     if usuario=="python" and senha=="fiap":
+        print(f'master logado')
+            
+        while True:
+                    
+                chave = input("Digite o nome do medico: ")
+                codigo = input("Digite o código do medico: ")
+               
+
+                medicos[chave] = codigo
+
+                print(f"\nNovo medico adicionado:")
+                print(f"Nome: {chave}")
+                print(f"Código: {codigo}")
+                                
+                op=int(input("digite 1 para continuar ou 0 para sair: "))
+                if op==0:
+                 break
+                    
+     else:
+      print(f'master não encontrado')
+     
+    
 
 
-opcao = int(input("Digite:\n1 para cadastrar usuário\n2 fazer login\n3 para ver o histórico de atividades\n4 para adicionar tarefa (necessita login de médico)\n5 para adicionar pacientes (necessita login de médico)\n6 para deslogar\n7 para acesso a camera do paciente\n0 para sair\n"))
+
+opcao = int(input("Digite:\n1 para cadastrar usuário\n2 fazer login\n3 para ver o histórico de atividades\n4 para adicionar tarefa (necessita login de médico)\n5 para adicionar pacientes (necessita login de médico)\n6 para deslogar\n7 para acesso a camera do paciente\n8 para addicionar medico (master necessario)\n9 para ver medicos(master necessario)\n0 para sair\n"))
 
 while opcao != 0:
     match opcao:
@@ -143,8 +168,21 @@ while opcao != 0:
                     print("paciente encontrado")
             else:
                 print("faça login primeiro")
+        case 8:
+            criar_medico()
+        case 9:
+             usuario=input("digite o login:")
+             senha=input("digite a sua senha:")
+             if  usuario=="python" and senha=="fiap":
+                    print(f'master logado')
+                    print("Lista de Médicos:")
+                    for nome, crm in medicos.items():
+                        print(f"{nome.capitalize()}: CRM {crm}")
+             else:
+                print(f'master não encontrado')
+     
         case _:
             print("opção invalida")
-
-    opcao = int(input("Digite:\n1 para cadastrar usuário\n2 fazer login\n3 para ver o histórico de atividades\n4 para adicionar tarefa (necessita login de médico)\n5 para adicionar pacientes (necessita login de médico)\n6 para deslogar\n7 para acesso a camera do paciente\n0 para sair\n"))
+        
+    opcao = int(input("Digite:\n1 para cadastrar usuário\n2 fazer login\n3 para ver o histórico de atividades\n4 para adicionar tarefa (necessita login de médico)\n5 para adicionar pacientes (necessita login de médico)\n6 para deslogar\n7 para acesso a camera do paciente\n8 para addicionar medico(master necessario)\n9 para ver medicos(master necessario)\n0 para sair\n"))
     
